138f6aa9ac61bfa9794260ef79e8b32519228829
nellerama <denelleoneil@gmail.com>
2016-05-01 18:52:46 -0400

    Added files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,17 +1,17 @@
 package com.example.prototimer;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v7.app.ActionBarActivity;
-import android.text.Editable;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.EditText;
 
 public class MainActivity extends ActionBarActivity {
-
+	CountDownTimer work;
+	CountDownTimer rest;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -37,39 +37,117 @@ public class MainActivity extends ActionBarActivity {
 		return super.onOptionsItemSelected(item);
 	}
 
-	public void startTimer(View v)
+	private long getTotalTime()
 	{
-		final EditText min= (EditText) findViewById(R.id.editTextMinutes);
-		final EditText sec= (EditText) findViewById(R.id.editTextSeconds);
-		//get values entered by user in the form of editables 
-		long seconds=0, minutes=0;
-		
+		EditText workSec= (EditText)findViewById(R.id.editTextWorkSeconds);
+		EditText workMin= (EditText)findViewById(R.id.editTextWorkMinutes);
+		EditText restSec= (EditText)findViewById(R.id.editTextRestSeconds);
+		EditText restMin= (EditText)findViewById(R.id.editTextRestMinutes);
+
+		int wSec=0, wMin=0, rSec=0, rMin=0, round=0, count=0, totalTime=0;
+
 		//convert editables to long
-		if(min.getText()!=null)
-			minutes=Integer.parseInt((sec.getText().toString()))*60;
-		if(sec.getText()!=null)
-			seconds= Integer.parseInt((min.getText().toString()));
+		if(workMin.getText().toString().trim().length()!=0)
+			wMin=Integer.parseInt((workMin.getText().toString()))*60;
+		if(workSec.getText().toString().trim().length()!=0)
+			wSec= Integer.parseInt((workSec.getText().toString()));
 
-			System.out.println("Seconds: "+seconds);
-		System.out.println("inutes: "+minutes);
-		//calculate final time by adding milliseconds
-		long time= (seconds+minutes)*1000;
+		if(restMin.getText().toString().trim().length()!=0)
+			rMin=Integer.parseInt((restMin.getText().toString()))*60;
+		if(restSec.getText().toString().trim().length()!=0)
+			rSec= Integer.parseInt((restSec.getText().toString()));
 
-		final ViewGroup vg= (ViewGroup)findViewById(R.id.LinearLayout);
-		new CountDownTimer(time+1000, 1000)
+		totalTime=wSec+wMin+rSec+rMin;
+		long timeMilli= totalTime*1000;
+		return timeMilli;
+	}
+
+	public void timer(View v)
+	{
+		//get user enterd values
+		EditText workSec= (EditText)findViewById(R.id.editTextWorkSeconds);
+		EditText workMin= (EditText)findViewById(R.id.editTextWorkMinutes);
+		EditText restSec= (EditText)findViewById(R.id.editTextRestSeconds);
+		EditText restMin= (EditText)findViewById(R.id.editTextRestMinutes);
+		EditText rounds= (EditText)findViewById(R.id.editTextRounds);
+
+		final MediaPlayer beepWork= MediaPlayer.create(getApplicationContext(), R.raw.beep_work);
+		final MediaPlayer beepRest= MediaPlayer.create(getApplicationContext(), R.raw.beep_rest);
+		final MediaPlayer beepEnd= MediaPlayer.create(getApplicationContext(), R.raw.beep_end);
+		
+		final int wMin, rSec,wSec, rMin, totalTime,round;
+		if(workMin.getText().toString().trim().length()!=0)
+			wMin=Integer.parseInt((workMin.getText().toString()))*60;
+		else
+			wMin=0;
+		if(workSec.getText().toString().trim().length()!=0)
+			wSec= Integer.parseInt((workSec.getText().toString()));
+		else
+			wSec=0;
+		if(restMin.getText().toString().trim().length()!=0)
+			rMin=Integer.parseInt((restMin.getText().toString()))*60;
+		else
+			rMin=0;
+		if(restSec.getText().toString().trim().length()!=0)
+			rSec= Integer.parseInt((restSec.getText().toString()));
+		else 
+			rSec=0;
+		if(rounds.getText().toString().trim().length()!=0)
+			round=Integer.parseInt(rounds.getText().toString());
+		else
+			round=0;
+
+		//convert editables to long
+		final int wCount=wSec+wMin;
+		final int rCount=rSec+rMin;
+
+		new CountDownTimer(getTotalTime()*round,975)
 		{
-			public void onTick(long millisUntilFinished)
+			int workCheck=wCount;
+			int restCheck= rCount;
+			int roundCheck=round;
+			@Override
+			public void onTick(long millisUntilFinished) 
 			{
-				System.out.println("tick..."+(millisUntilFinished-1000)/1000%60);
-				min.setText((millisUntilFinished-1000)/60/1000+"");
-				sec.setText(((millisUntilFinished-1000)/1000%60)+"");
-				vg.invalidate();
+
+				setContentView(R.layout.activity_timer);
+				if(workCheck>0)
+				{
+					beepWork.start();
+					((EditText) findViewById(R.id.editTextIntervalTime)).setText(""+workCheck);
+					((EditText) findViewById(R.id.editTextIntervalType)).setText(""+"Work");
+					((EditText) findViewById(R.id.editTextRoundValue)).setText(""+roundCheck);
+					workCheck--;
+				}
+				else if(restCheck>0)
+				{
+					beepWork.pause();
+					((EditText) findViewById(R.id.editTextIntervalTime)).setText(""+restCheck);
+					((EditText) findViewById(R.id.editTextIntervalType)).setText(""+"Rest");
+					((EditText) findViewById(R.id.editTextRoundValue)).setText(""+roundCheck);
+					restCheck--;
+				}
+				else
+				{
+					beepWork.start();
+					roundCheck--;
+					workCheck=wCount;
+					restCheck=rCount;
+					((EditText) findViewById(R.id.editTextIntervalTime)).setText(""+workCheck);
+					((EditText) findViewById(R.id.editTextIntervalType)).setText(""+"Work");
+					((EditText) findViewById(R.id.editTextRoundValue)).setText(""+roundCheck);
+					workCheck--;
+				}
 			}
 
-			public void onFinish()
+			@Override
+			public void onFinish() 
 			{
-				System.out.println("Finished");
+				MediaPlayer beepWork= MediaPlayer.create(getApplicationContext(), R.raw.beep_end);
+				beepWork.start();
+				setContentView(R.layout.finished_layout);
 			}
 		}.start();
 	}
+
 }
